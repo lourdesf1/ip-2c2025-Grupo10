d99e8c37054dfbcde1ffdcbd3993fc812c4dae22
lourdesf1 <floresluly135@gmail.com>
2025-11-13 16:55:51 -0300

    agregue hola a clase burbujeo

--- visualizador/algorithms/sort_bubble.py
+++ visualizador/algorithms/sort_bubble.py
@@ -14,7 +14,7 @@ def init(vals):
 
 def step():
     # TODO:
-    # 1) Elegir índices a y b a comparar en este micro-paso (según tu Bubble).
+    # hola1) Elegir índices a y b a comparar en este micro-paso (según tu Bubble).
     # 2) Si corresponde, hacer el intercambio real en items[a], items[b] y marcar swap=True.
     # 3) Avanzar punteros (preparar el próximo paso).
     # 4) Devolver {"a": a, "b": b, "swap": swap, "done": False}.
